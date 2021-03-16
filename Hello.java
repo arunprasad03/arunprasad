d44f4fa06e87b4798e58ac68d639c6b187dc9797
arunprasad03 <60088948+arunprasad03@users.noreply.github.com>
2021-03-16 09:03:30 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,7 +1,17 @@
 import java.io.*;
 public class Hello {
     public static void main(String[] args) {
-        for(int i=0;i<10;i++)
+        for(int i=0;i<10;i++){
         System.out.println(" Hello World ... " + i);
+		}
+		validate(13);
+	
     }
+	
+	static void validate(int age){  
+     if(age<18)  
+      throw new ArithmeticException("not valid");  
+     else  
+      System.out.println("welcome to vote");  
+   }  
 }
